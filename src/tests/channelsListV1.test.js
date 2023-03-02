d0e77a41ae84025edc732b20bcbbdf6e44edfe91
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-03-02 17:50:00 +1100

    add minor changes to channelsListV1.test

--- src/tests/channelsListV1.test.js
+++ src/tests/channelsListV1.test.js
@@ -1,4 +1,5 @@
-import {channelsListV1,channelsCreateV1} from '../channels';
+import {channelsListV1, channelsCreateV1} from '../channels';
+import {authRegisterV1} from '../auth.js';
 import {clearV1} from './other';
 
 test('invalid user id', () => {
@@ -9,15 +10,15 @@ test('invalid user id', () => {
 test('channelsList one channels ', () => {
     clearV1();
     const user1 = authRegisterV1('oliver@gmail.com','123456','Oliver','Lu');
-    const channel1 = channelsCreateV1(user1,'test1',true);
-    expect(channelsListV1(user)).toStrictEqual({channels:[{channelId:1, name:test1}]})
+    const channel1 = channelsCreateV1(user1.authUserId,'test1',true);
+    expect(channelsListV1(user)).toStrictEqual({channels:[{channelId:channel1.channelId, name:test1}]})
 })
 
 test('channelsList three channels ', () => {
     clearV1();
     const user1 = authRegisterV1('oliver@gmail.com','123456','Oliver','Lu');
-    const channel1 = channelsCreateV1(user1,'test1',true);
-    const channel2 = channelsCreateV1(user1,'test2',true);
-    const channel3 = channelsCreateV1(user1,'test3',true);
-    expect(channelsListV1(user)).toStrictEqual({channels:[{channelId:0, name:test1}, {channelId:1, name:test2}, {channelId:2, name:test3}]})
+    const channel1 = channelsCreateV1(user1.authUserId,'test1',true);
+    const channel2 = channelsCreateV1(user1.authUserId,'test2',true);
+    const channel3 = channelsCreateV1(user1.authUserId,'test3',true);
+    expect(channelsListV1(user1)).toStrictEqual({channels:[{channelId:channel1.channelId, name: 'test1'}, {channelId:channel2.channelId, name: 'test2'}, {channelId:channel3.channelId, name: 'test3'}]})
 })
