ac3f62ee1b235cf4cdb808831b97e85e1810ce4b
Akanksha Sood <z5421482@ad.unsw.edu.au>
2023-03-02 11:58:03 +1100

    Added tests for authLoginV1.test.js

---
+++ src/tests/authLoginV1.test.js
@@ -0,0 +1,27 @@
+import { authLoginV1, authRegisterV1 } from '../auth.js';
+
+describe('Valid inputs', () => {
+    test('Valid return type (object)', () => {
+        const register1 = authRegisterV1('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
+        const login1 = authLoginV1('alice.sith@gmail.com', '123456');
+        expect(login1).toHaveProperty(authUserId);
+    });
+    test('Valid userId', () => {
+        const register1 = authRegisterV1('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
+        const login1 = authLoginV1('alice.sith@gmail.com', '123456');
+        expect(register1.authUserId).toEqual(login1.authUserId);
+    });
+});
+
+describe('Invalid inputs', () => {
+    test('Email does not belong to a user', () => {
+        const register1 = authRegisterV1('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
+        const login1 = authLoginV1('bob.langford@gmail.com', '123456');
+        expect(login1).toEqual({error: expect.any(String)});
+    });
+    test('Incorrect password', () => {
+        const register1 = authRegisterV1('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
+        const login1 = authLoginV1('alice.sith@gmail.com', 'password');
+        expect(login1).toEqual({error: expect.any(String)});
+    });
+});
