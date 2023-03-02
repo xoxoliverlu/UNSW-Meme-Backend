0e74e1c3d67654eb2f0bcd3c8b31a51c36723d5f
Akanksha Sood <z5421482@ad.unsw.edu.au>
2023-03-02 12:10:07 +1100

    Delete duplicate test file and update error test in authregisterV1

--- src/tests/authRegister.test.js
+++ src/tests/authRegister.test.js
@@ -27,26 +27,26 @@ describe('Invalid inputs', () => {
         const register2 = authRegisterV1('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
         const register3 = authRegisterV1('alice.smith@gmail.com', '123456', 'Bob', 'Langford');
         const register4 = authRegisterV1('Alice.Smith@gmail.com', '123456', 'Alice', 'Smith');
-        expect(register2).toEqual({error: 'error'});
-        expect(register3).toEqual({error: 'error'});
-        expect(register4).toEqual({error: 'error'});
+        expect(register2).toEqual({error: expect.any(String)});
+        expect(register3).toEqual({error: expect.any(String)});
+        expect(register4).toEqual({error: expect.any(String)});
     });
     test('Password less than 6 characters', () => {
         const register1 = authRegisterV1('alice.smith@gmail.com', 'Pass', 'Alice', 'Smith');
         const register2 = authRegisterV1('bob.langford@gmail.com', '12$>p', 'Bob', 'Langford');
-        expect(register1).toEqual({error: 'error'});
-        expect(register2).toEqual({error: 'error'});
+        expect(register1).toEqual({error: expect.any(String)});
+        expect(register2).toEqual({error: expect.any(String)});
     });
     test('nameFirst is empty or > 50 characters', () => {
         const register1 = authRegisterV1('alice.smith@gmail.com', 'password', ' ', 'Smith');
         const register2 = authRegisterV1('bob.langford@gmail.com', '123456', 'ABCDEFGhijklmnopqrstuvwxyzABCDEFGhijklmnopqrstuvwxyzABCDEFGhijklmnopqrstuvwxyz', 'Langford');
-        expect(register1).toEqual({error: 'error'});
-        expect(register2).toEqual({error: 'error'});
+        expect(register1).toEqual({error: expect.any(String)});
+        expect(register2).toEqual({error: expect.any(String)});
     });
     test('nameLast is empty or > 50 characters', () => {
         const register1 = authRegisterV1('alice.smith@gmail.com', 'password', 'Alice', ' ');
         const register2 = authRegisterV1('bob.langford@gmail.com', '123456', 'Bob', 'ABCDEFGhijklmnopqrstuvwxyzABCDEFGhijklmnopqrstuvwxyzABCDEFGhijklmnopqrstuvwxyz');
-        expect(register1).toEqual({error: 'error'});
-        expect(register2).toEqual({error: 'error'});
+        expect(register1).toEqual({error: expect.any(String)});
+        expect(register2).toEqual({error: expect.any(String)});
     });
 });
