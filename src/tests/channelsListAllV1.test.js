93f267bf38d77b4dea7a7115e0f937ffb6b569df
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-03-02 17:33:09 +1100

    test updated

--- src/tests/channelsListAllV1.test.js
+++ src/tests/channelsListAllV1.test.js
@@ -4,19 +4,19 @@ import {channelsListAllV1} from './channels';
 test('Invalid users', () => {
     clearV1();
     const user1 = authRegisterV1('oliver@gmail.com','123456','oliver','lu')
-    expect(channelsListAllV1(12389)).toStrictEqual({error: 'error'});
+    expect(channelsListAllV1((user1.authUserId))+1).toStrictEqual({error: 'error'});
 })
 
 test('multiple channels', () => {
     clearV1();
     const user1 = authRegisterV1('oliver@gmail.com','123456','oliver','lu')
-    const channel1 = channelsCreateV1(user1,"test1",true);
-    const channel2 = channelsCreateV1(user1,"test2",true);
-    const channel3 = channelsCreateV1(user1,"test3",true);
+    const channel1 = channelsCreateV1(user1.authUserId,"test1",true);
+    const channel2 = channelsCreateV1(user1.authUserId,"test2",true);
+    const channel3 = channelsCreateV1(user1.authUserId,"test3",true);
     expect(channelsListAllV1).toStrictEqual({channels: [
-        {channelId: channel1, name: 'test1'},
-        {channelId: channel2, name: 'test2'},
-        {channelId: channel3, name: 'test3'},
+        {channelId: channel1.channelId, name: 'test1'},
+        {channelId: channel2.channelId, name: 'test2'},
+        {channelId: channel3.channelId, name: 'test3'},
     ]})
 })
 
