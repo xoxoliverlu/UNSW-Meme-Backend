7bdd92cc601a92407d958feb2e62720c54ff9cab
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-03-03 12:39:32 +1100

    finish fixing channelsListV1.test.js

--- src/tests/channelsListV1.test.js
+++ src/tests/channelsListV1.test.js
@@ -7,19 +7,20 @@ beforeEach(() => {
 });
 
 test('invalid user id', () => {
-    expect(channelsListV1(-135)).toStrictEqual({error: 'error'});
+    const user1 = authRegisterV1('oliver@gmail.com','123456789','Oliver','Lu');
+    expect(channelsListV1(user1.authUserId + 1)).toStrictEqual({error: 'error'});
 });
 
 test('channelsList one channels ', () => {
-    const user1 = authRegisterV1('oliver@gmail.com','123456','Oliver','Lu');
+    const user1 = authRegisterV1('oliver@gmail.com','123456789','Oliver','Lu');
     const channel1 = channelsCreateV1(user1.authUserId,'test1',true);
-    expect(channelsListV1(user1)).toStrictEqual({channels:[{channelId:channel1.channelId, name:test1}]});
+    expect(channelsListV1(user1.authUserId)).toStrictEqual({channels:[{channelId:channel1.channelId, name: 'test1' }]});
 });
 
 test('channelsList three channels ', () => {
-    const user1 = authRegisterV1('oliver@gmail.com','123456','Oliver','Lu');
+    const user1 = authRegisterV1('oliver@gmail.com','123456789','Oliver','Lu');
     const channel1 = channelsCreateV1(user1.authUserId,'test1',true);
     const channel2 = channelsCreateV1(user1.authUserId,'test2',true);
     const channel3 = channelsCreateV1(user1.authUserId,'test3',true);
-    expect(channelsListV1(user1)).toStrictEqual({channels:[{channelId:channel1.channelId, name: 'test1'}, {channelId:channel2.channelId, name: 'test2'}, {channelId:channel3.channelId, name: 'test3'}]});
+    expect(channelsListV1(user1.authUserId)).toStrictEqual({channels:[{channelId:channel1.channelId, name: 'test1'}, {channelId:channel2.channelId, name: 'test2'}, {channelId:channel3.channelId, name: 'test3'}]});
 });
