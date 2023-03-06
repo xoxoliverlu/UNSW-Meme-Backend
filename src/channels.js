a45415c06381ce5c9d9e01c6a8814ef76ffa2577
Wei-Cheng Lu <wei-cheng.lu@student.unsw.edu.au>
2023-03-06 12:56:02 +1100

    Revert "Merge branch 'iteration_1/channelInviteV1_tests' into 'master'"
    This reverts merge request !43

--- src/channels.js
+++ src/channels.js
@@ -1,59 +1,111 @@
-function channelsCreateV1(authUserId, name, isPublic) {
-        const data = getData();
-        let userKeys = Object.keys(data.users);
-        let channelNames = Object.keys(data.channels);
-        let validUser = false;
-        let memberEmail = {};
-        
-        // Check that the length of name is more than 1 or less than 20 characters
-        for (const user of userKeys) {  
-          if (data.users[user].authUserId === authUserId) {
-            validUser = true;
-            memberEmail = data.users[user];
-          }
-        }
-        if (validUser === false) {
-          return {
-            error: 'User is not valid'
-          }
-        }
-        if (name.length < 1) {
-          return {
-            error: 'channel name is too short'
-          }
-        }  
-        if (name.length > 20) {
-          return {
-            error: 'channel name is too long'
-          }
-        }
-        
-        // Check for any duplicate names
-        for (const channel of channelNames) {
-          if (name === channel) {
-            return {
-              error: 'Channel name already exists'
-            }
-          }
-        }
-      
-        let cId = channelNames.length;
-      
-        // Assign information to the new channel
-        data.channels[name] = {
-          channelId: cId, 
-          isPublic: isPublic,
-          allMembers: [memberEmail],
-          ownerMembers: [memberEmail]
-        };
-      
-        setData(data);
+import { getData,setData } from "./dataStore";
+
+  export function channelsCreateV1(authUserId, name, isPublic){
+    const data = getData();
+    let validUser = false;
+    
+    // Check that the length of name is more than 1 or less than 20 characters
+    for (const user of data.users) {  
+      if (user.uId === authUserId) {
+        validUser = true;
+      }
+    }
+    if (validUser === false) {
+      return {
+        error: 'error'
+      }
+    }
+    if (name.length < 1) {
+      return {
+        error: 'error'
+      }
+    }  
+    if (name.length > 20) {
+      return {
+        error: 'error'
+      }
+    }
+    
+    // Check for any duplicate names
+    for (const channel of data.channels) {
+      if (name === channel.name) {
         return {
-          channelId:  data.channels[name].channelId
+          error: 'error'
         }
- // Provides an array of all channels (and their associated details)
-// that the authorised user is part of.
-      }  
+      }
+    }
+  
+    let Id = 0;
+    if (data.channels.length === 0) {
+        Id = 0;
+    } else {
+        Id = data.channels[data.channels.length - 1] + 1;
+    }
+    // Assign information to the new channel
+    data.channels.push({
+      channelId: Id, 
+      name: name,
+      isPublic: isPublic,
+      allMembers: [authUserId],
+      ownerMembers: [authUserId]
+    })
+  
+    setData(data);
+    return {
+      channelId: Id
+    }
+  }  
+
+
+export function channelsListV1(authUserId){
+  const data = getData();
+  let validId = false;
+
+  for (let user of data.users){
+    if (user.uId === authUserId){
+      validId = true
+    }
+  }
+  
+  if (!validId){
+    return {error: 'error'};
+  }
+
+  let associatedChannels = [];
+  
+  for (let channel of data.channels){
+    if (channel.allMembers.includes(authUserId)){
+      associatedChannels.push({channelId:channel.channelId, name:channel.name})
+    }
+  }
+
+  return {channels: associatedChannels};
       
-     
-      
+}
+
+export function channelsListAllV1(authUserId){
+  const data = getData();
+  let validId = false;
+
+  for (let user of data.users){
+    if (user.uId === authUserId){
+      validId = true
+    }
+  }
+  
+  if (!validId){
+    return {error: 'error'};
+  }
+  
+  let result = [];
+
+  for (let channel of data.channels){
+    result.push({channelId: channel.channelId, name: channel.name})  
+  }
+  
+  return {
+    channels: result,
+  };
+}
+
+
