f9b6bdb7bab38bcd5b35a0e428dd4ce9aee36d93
Dimpi Garnepudi <z5419209@ad.unsw.edu.au>
2023-03-06 20:51:28 +1100

    function implementation

--- src/channel.js
+++ src/channel.js
@@ -88,25 +88,192 @@ export function channelJoinV1(authUserId, channelId) {
     }
 }
 
-function channelInviteV1(authUserId, channelId, uId){
+export function channelInviteV1(authUserId, channelId, uId){
+  const data = getData();
+  let valid = false;
+  let userInfo = '';
+
+  // Check that channelId refers to a valid channel
+  for (const channel of channelKeys) {
+    if (data.channels[channel].channelId === channelId) {
+      valid = true; 
+    }
+  }
+  if (valid === false) {
     return {
+      error: 'error'
+    }
+  }
+
+  // Check that uId refers to a valid user
+  valid = false 
+  for (const user of userKeys) {
+    if (data.users[user].authUserId === uId) {
+      valid = true;
+      userInfo = user;
 
     }
+  }
+  if (valid === false) {
+    return {
+      error: 'error'
+    }
+  }
+
+  // Check that authUserId is valid
+  valid = false;
+  for (const user of userKeys) {
+    if (data.users[user].authUserId === authUserId) {
+      valid = true;
+    }
+  }
+  if (valid === false) {
+    return {
+      error: 'error'
+    }
+  }
+
+  // Check that user is not already a member of the channel
+  for (const channel of channelKeys) {
+    for (const id of data.channels[channel].allMembers) {
+      if (id.authUserId === uId) {
+        return {
+          error: 'error'
+        }
+      }  
+    }
+  }
+
+  // check that the authorised user is a member of the channel
+  valid = false;
+  for (const channel of channelKeys) {
+    for (const aId of data.channels[channel].ownerMembers) {
+      if (aId.authUserId === authUserId && channelId === data.channels[channel].channelId) {
+        valid = true;
+      }
+      if (authUserId === 0) {
+        valid = true;
+      }
+    }  
+  }
+  if (!valid) {
+    return {
+      error: 'error'
+    }
+  }
+
+  // Add invited user to the channel
+  for (const channel of channelKeys) {
+    if (data.channels[channel].channelId === channelId) {
+      data.channels[channel].allMembers.push(data.users[userInfo]);
+    }
+  }
+  return {
+
+  }
 }
 
-function channelMessagesV1(authUserId, channelId, start){
-    return {
-        
-            messages: [
-              {
-                messageId: 1,
-                uId: 1,
-                message: 'Hello world',
-                timeSent: 1582426789,
-              }
-            ],
-            start: 0,
-            end: 50,
-        
+export function channelMessagesV1(authUserId, channelId, start){
+const data = getData();
+let valid = false;   
+let channelPosition;
+
+// ERROR CHECKING
+// Check if channelId refers to a valid channel
+for (const channel of channelKeys) {  
+if (data.channels[channel].channelId === channelId) {
+    valid = true;
+    channelPosition = channel;
+}      
+}
+
+if (valid === false) {
+return {
+    error: 'error'
+}
+}
+
+// Check if authUserId refers to a valid user
+valid = false;
+for (const user of userKeys) {
+if (data.users[user].authUserId === authUserId) {
+    valid = true;
+}  
+}
+
+if (valid === false) {
+return {
+    error: 'error'
+}
+}
+// Check if the user is a member of the given channel
+valid = false;
+for (const channel of channelKeys) {
+for (const aId of data.channels[channel].allMembers) {
+    if (aId.authUserId === authUserId && channelId === data.channels[channel].channelId) {
+    valid = true; 
     }
+    
+}  
+}
+if (!valid) {
+return {
+    error: 'error'
+}
+} 
+
+// See how many messages there are in the channel
+let endIndex;
+const channelMessages = {
+messages: [],
+start: start,
+end: endIndex, 
+}
+
+let numberOfMessages;
+
+if (typeof data.channels[channelPosition].messages === 'undefined') {
+numberOfMessages = 0;
+} else {
+numberOfMessages = data.channels[channelPosition].messages.length();
+}
+
+// Check if starting index is not greater than the total number
+// of messages in the channel
+if (start > numberOfMessages) { 
+return {
+    error: 'error'
+}
+}
+
+// Loop through start + 50 messages and return details of each message
+if (numberOfMessages === 0) {
+channelMessages.end = -1;
+return channelMessages;
+} else {
+if (numberOfMessages > start + 50) {
+    for (let i = start; i < start + 50; i++) {
+    const newMessage = {
+        messageId: data.channels[channelPosition].messages[i].messageId,
+        uId: data.channels[channelPosition].messages[i].uId,
+        message: data.channels[channelPosition].messages[i].message,
+        timeSent: data.channels[channelPosition].messages[i].timeSent
+    }
+    }
+    channelMessages.messages.push(newMessage);
+    channelMessages.end = i;
+} else {
+    for (let i = start; i < numberOfMessages - start; i++) {
+    const newMessage = {
+        messageId: data.channels[channelPosition].messages[i].messageId,
+        uId: data.channels[channelPosition].messages[i].uId,
+        message: data.channels[channelPosition].messages[i].message,
+        timeSent: data.channels[channelPosition].messages[i].timeSent
+    }
+    }
+    channelMessages.messages.push(newMessage);
+    channelMessages.end = i;
+}
+}
+return channelMessages;
 }
