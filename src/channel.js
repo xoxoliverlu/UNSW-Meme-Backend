2dd4a0aad3dec7a35b78e74e99d57dd6b4e1c450
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-03-10 14:02:49 +1100

    fix channel messages implementation

--- src/channel.js
+++ src/channel.js
@@ -209,86 +209,86 @@ export function channelInviteV1(authUserId, channelId, uId){
  * @returns {object} - error if user id and channelid are invalid or start index is > 50
  */
 export function channelMessagesV1(authUserId, channelId, start){
-const data = getData();
-let validChannel = false;
-const channelKeys = data.channels;
-let channelInfo;
+  const data = getData();
+  let validChannel = false;
+  let channelInfo;
 
-// ERROR CHECKING
-// Check if channelId refers to a valid channel
-for (const channel of data.channels) {
-  if (channel.channelId === channelId) {
-    validChannel = true;
-    channelInfo = channel;
+  // ERROR CHECKING
+  // Check if channelId refers to a valid channel
+  for (const channel of data.channels) {
+    if (channel.channelId === channelId) {
+      validChannel = true;
+      channelInfo = channel;
+    }
   }
-}
 
-if (validChannel === false) {
-return {
-    error: 'error'
-}
-}
-
-// Check if authUserId refers to a valid user
-let validAuthuserId = false;
-for (const user of data.users) {
-  if (user.uId === authUserId) {
-    validAuthuserId = true;
-  }
-}
-
-if (validAuthuserId === false) {
-  return {
-    error: 'error'
-  }
-}
-// Checking if authuserId is a member of the given channel
-let isMember = false;
-for (const member of channelInfo.allMembers) {
-  if (member === authUserId) {
-    isMember = true;
-  }
-}
-if (isMember === false) {
-  return {
-    error: 'error'
-}
-}
-
-// Check if starting index is not greater than the total number
-// of messages in the channel
-if (start > channelInfo.messages.length) {
+  if (validChannel === false) {
   return {
       error: 'error'
   }
-}
+  }
 
-// checks if start is at the end
-if (start === channelInfo.messages.length) {
+  // Check if authUserId refers to a valid user
+  let validAuthuserId = false;
+  for (const user of data.users) {
+    if (user.uId === authUserId) {
+      validAuthuserId = true;
+    }
+  }
+
+  if (validAuthuserId === false) {
+    return {
+      error: 'error'
+    }
+  }
+  // Checking if authuserId is a member of the given channel
+  let isMember = false;
+  for (const member of channelInfo.allMembers) {
+    if (member === authUserId) {
+      isMember = true;
+    }
+  }
+  if (isMember === false) {
+    return {
+      error: 'error'
+  }
+  }
+
+  // Check if starting index is not greater than the total number
+  // of messages in the channel
+  if (start > channelInfo.messages.length) {
+    return {
+        error: 'error'
+    }
+  }
+
+  // checks if start is at the end
+  if (start === channelInfo.messages.length) {
+    return {
+      messages:channelInfo.messages[channelInfo.messages.length-1],
+      start: start,
+      end: -1,
+    }
+  }
+  let messagesArray = [];
+  let newEnd;
+
+  for (let i = start; i < start + 50; i++) {
+    if(i === channelInfo.messages.length - 1) {
+      newEnd = -1;
+      break;
+    }
+    messagesArray.push(channelInfo.messages[i])
+  }
+  
+  if (newEnd !== -1) {
+    newEnd = start + 50;
+  }
   return {
-    messages:[],
+    messages: messagesArray,
     start: start,
-    end: -1,
+    end: newEnd,
   }
-}
-let messagesArray = [];
-let newEnd;
-
-for (let i = start; i < start + 50; i++) {
-  if(channelInfo.messages.length === 50) {
-    newEnd = -1;
-    break;
-  }
-  messagesArray.push(channelInfo.messages[i])
-}
-if (newEnd !== -1) {
-  newEnd = start + 50;
-}
-return {
-  messages: messagesArray,
-  start: start,
-  end: newEnd,
-}
 
 }
 
