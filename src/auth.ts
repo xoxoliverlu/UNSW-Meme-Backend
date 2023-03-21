53f337e8316e04bda58e2487ad2b1f6740574d25
Akanksha Sood <z5421482@ad.unsw.edu.au>
2023-03-22 00:46:11 +1100

    Use double quotes instead of single quotes

--- src/auth.ts
+++ src/auth.ts
@@ -9,6 +9,7 @@ type authUserId = {
 
 };
 
+
 type tokenReturn = string;
 type handleReturn = string;
 /**
@@ -41,7 +42,7 @@ const authLoginV1 = (email: string, password: string): authUserId => {
 
 const authRegisterV2 = (email: string, password: string, nameFirst: string, nameLast: string): authUserId => {
   const register = authRegisterV1(email, password, nameFirst, nameLast);
-  if (register.hasOwnProperty('authUserId')) {
+  if (register.hasOwnProperty("authUserId")) {
     const token = generateToken(register.authUserId);
     return {
       token: token,
@@ -184,5 +185,4 @@ const generateHandle = (nameFirst: string, nameLast: string): handleReturn => {
   }
   return newHandle;
 }
-
 export { authRegisterV2, authLoginV1 };
