437d8b56a93f242b8b0c007311272849a121ad99
Akanksha Sood <z5421482@ad.unsw.edu.au>
2023-03-21 20:34:06 +1100

    Rename authRegisterV1 to authRegisterV2

--- src/tests/clearV1.test.ts
+++ src/tests/clearV1.test.ts
@@ -1,19 +1,19 @@
 import { clearV1 } from '../other';
-import { authRegisterV1, authLoginV1 } from '../auth';
+import { authRegisterV2, authLoginV1 } from '../auth';
 
 describe('Test for a clear dataObject', () => {
   test('Registering users in a clear dataObject', () => {
-    const user1 = authRegisterV1('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
-    const user2 = authRegisterV1('bob.langford@gmail.com', '123456', 'Bob', 'Langford');
+    const user1 = authRegisterV2('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
+    const user2 = authRegisterV2('bob.langford@gmail.com', '123456', 'Bob', 'Langford');
     clearV1();
-    const user3 = authRegisterV1('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
-    const user4 = authRegisterV1('bob.langford@gmail.com', '123456', 'Bob', 'Langford');
+    const user3 = authRegisterV2('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
+    const user4 = authRegisterV2('bob.langford@gmail.com', '123456', 'Bob', 'Langford');
     expect(user3).toHaveProperty('authUserId');
     expect(user4).toHaveProperty('authUserId');
   });
   test('User deleted after clear', () => {
-    const user1 = authRegisterV1('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
-    const user2 = authRegisterV1('bob.langford@gmail.com', '123456', 'Bob', 'Langford');
+    const user1 = authRegisterV2('alice.smith@gmail.com', 'password', 'Alice', 'Smith');
+    const user2 = authRegisterV2('bob.langford@gmail.com', '123456', 'Bob', 'Langford');
     clearV1();
     const user1Id = authLoginV1('alice.smith@gmail.com', 'password');
     const user2Id = authLoginV1('bob.langford@gmail.com', '123456');
