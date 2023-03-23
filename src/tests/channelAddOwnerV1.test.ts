9153438775a84993eaed6a8cdeecd202c9ea4bde
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-03-23 16:31:06 +1100

    add helper functions into test

--- src/tests/channelAddOwnerV1.test.ts
+++ src/tests/channelAddOwnerV1.test.ts
@@ -1,4 +1,9 @@
-import { requestAuthLogin, requestAuthRegister } from "../requests";
+import {
+  requestAuthLogin,
+  requestAuthRegister,
+  requestChannelAddOwner,
+  requestChannelsCreate,
+} from "../requests";
 import { port, url } from "./config.json";
 const request = require("sync-request");
 
@@ -7,29 +12,21 @@ beforeEach(() => {
 });
 
 test("success addOwner", () => {
-  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu")
-  requestAuthRegister("oliverwluu@gmail.com","cl3cl3vul44","Oliver","Lu",)
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("oliverwluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
 
-  let loginRes = requestAuthLogin("oliverwlu@gmail.com","cl3cl3vul4");
-  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com","cl3cl3vul44")
-  let { token: token1, authUserId: authUserId1 } = JSON.parse(loginRes.body);
-  let { token: token2, authUserId: authUserId2 } = JSON.parse(loginRes2.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1, authUserId: authUserId1 } = loginRes;
+  let { token: token2, authUserId: authUserId2 } = loginRes2;
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
+  let { channelId } = channelCreateRes;
   request("POST", `${url}:${port}/channel/invite/v2`, {
     token: token1,
     channlId: channelId,
     uId: authUserId2,
   });
-  request("POST", `${url}:${port}/channel/addowner/v1`, {
-    token: token1,
-    channelID: channelId,
-    uId: authUserId2,
-  });
+  requestChannelAddOwner(token1, channelId, authUserId2);
 
   let channelDetailsRes = request("POST", `${url}:${port}/channel/details/v2`, {
     token: token2,
@@ -37,283 +34,152 @@ test("success addOwner", () => {
     uId: authUserId2,
   });
 
-  let { ownerMembers } = JSON.parse(channelDetailsRes);
+  let { ownerMembers } = channelDetailsRes;
   expect(ownerMembers).toStrictEqual([authUserId1, authUserId2]);
 });
 
 test("error token", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1 } = JSON.parse(loginRes.body);
-  let { authUserId: authUserId2 } = JSON.parse(loginRes2.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("oliverwluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes;
+  let { authUserId: authUserId2 } = loginRes2;
+
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
+
+  let { channelId } = channelCreateRes;
+
   request("POST", `${url}:${port}/channel/invite/v2`, {
     token: token1,
     channlId: channelId,
     uId: authUserId2,
   });
+
   token1 += "error";
-  let channelAddOwneRes = request(
-    "POST",
-    `${url}:${port}/channel/addowner/v1`,
-    {
-      token: token1,
-      channelID: channelId,
-      uId: authUserId2,
-    }
+  let channelAddOwneRes = requestChannelAddOwner(
+    token1,
+    channelId,
+    authUserId2
   );
-  let { error } = JSON.parse(channelAddOwneRes);
+
+  let { error } = channelAddOwneRes;
   expect(error).toEqual(expect.any(String));
 });
 
-
 test("error userID", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let { token: token1, authUserId: authUserId1 } = JSON.parse(loginRes.body);
-  let { token: token2, authUserId: authUserId2 } = JSON.parse(loginRes2.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("oliverwluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes;
+  let { authUserId: authUserId2 } = loginRes2;
+
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
+
+  let { channelId } = channelCreateRes;
+
   request("POST", `${url}:${port}/channel/invite/v2`, {
     token: token1,
     channlId: channelId,
     uId: authUserId2,
   });
-  let channelAddOwneRes = request(
-    "POST",
-    `${url}:${port}/channel/addowner/v1`,
-    {
-      token: token1,
-      channelID: channelId,
-      uId: authUserId2 + 200,
-    }
+
+  let channelAddOwneRes = requestChannelAddOwner(
+    token1,
+    channelId,
+    authUserId2 + 200
   );
-  let { error } = JSON.parse(channelAddOwneRes);
+  let { error } = channelAddOwneRes;
   expect(error).toEqual(expect.any(String));
 });
 
 test("error invalid channelID", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1} = JSON.parse(loginRes.body);
-  let { authUserId: authUserId2 } = JSON.parse(loginRes2.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("oliverwluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes;
+  let { authUserId: authUserId2 } = loginRes2;
+
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
+
+  let { channelId } = channelCreateRes;
+
   request("POST", `${url}:${port}/channel/invite/v2`, {
     token: token1,
     channlId: channelId,
     uId: authUserId2,
   });
-  let channelAddOwneRes = request(
-    "POST",
-    `${url}:${port}/channel/addowner/v1`,
-    {
-      token: token1,
-      channelID: channelId+1,
-      uId: authUserId2,
-    }
+
+  let channelAddOwneRes = requestChannelAddOwner(
+    token1,
+    channelId + 1,
+    authUserId2
   );
-  let { error } = JSON.parse(channelAddOwneRes);
+  let { error } = channelAddOwneRes;
   expect(error).toEqual(expect.any(String));
 });
 
 test("error user not a part of channel ", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1 } = JSON.parse(loginRes.body);
-  let { authUserId: authUserId2 } = JSON.parse(loginRes2.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("oliverwluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
 
-  let channelAddOwneRes = request(
-    "POST",
-    `${url}:${port}/channel/addowner/v1`,
-    {
-      token: token1,
-      channelID: channelId,
-      uId: authUserId2,
-    }
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes;
+  let { authUserId: authUserId2 } = loginRes2;
+
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
+
+  let { channelId } = channelCreateRes;
+
+  let channelAddOwneRes = requestChannelAddOwner(
+    token1,
+    channelId,
+    authUserId2
   );
-  let { error } = JSON.parse(channelAddOwneRes);
+  let { error } = channelAddOwneRes;
   expect(error).toEqual(expect.any(String));
 });
 
-
 test("error uId is already an owner ", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
 
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let { token: token1, authuserId: authUserId1 } = loginRes;
 
-  let { token: token1, authUserId: authUserId1 } = JSON.parse(loginRes.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
 
-  let channelAddOwneRes = request(
-    "POST",
-    `${url}:${port}/channel/addowner/v1`,
-    {
-      token: token1,
-      channelID: channelId,
-      uId: authUserId1,
-    }
+  let { channelId } = channelCreateRes;
+
+  let channelAddOwneRes = requestChannelAddOwner(
+    token1,
+    channelId,
+    authUserId1
   );
-  let { error } = JSON.parse(channelAddOwneRes);
+  let { error } = channelAddOwneRes;
   expect(error).toEqual(expect.any(String));
 });
 
 test("error user doesn't have owner permission ", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverlu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul444",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverlu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let loginRes3 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul444" },
-  });
-  let { token: token1, authUserId: authUserId1 } = JSON.parse(loginRes.body);
-  let { token: token2, authUserId: authUserId2 } = JSON.parse(loginRes2.body);
-  let { token: token3, authUserId: authUserId3 } = JSON.parse(loginRes3.body);
-  let channelCreateRes = request("POST", `${url}:${port}/channels/create/v2`, {
-    token: token1,
-    name: "sampleChannel",
-    isPublic: true,
-  });
-  let { channelId } = JSON.parse(channelCreateRes);
+  requestAuthRegister("oliverwlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("oliverwluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+  requestAuthRegister("oliverwluuu@gmail.com", "cl3cl3vul444", "Oliver", "Lu");
+
+  let loginRes = requestAuthLogin("oliverwlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let loginRes3 = requestAuthLogin("olivrewluuu@gmail.com", "cl3cl3vul444");
+
+  let { token: token1, authUserId: authUserId1 } = loginRes;
+  let { token: token2, authUserId: authUserId2 } = loginRes2;
+  let { token: token3, authUserId: authUserId3 } = loginRes3;
+  let channelCreateRes = requestChannelsCreate(token1, "sampleChannel", true);
+
+  let { channelId } = channelCreateRes;
   request("POST", `${url}:${port}/channel/invite/v2`, {
     token: token1,
     channlId: channelId,
@@ -326,15 +192,12 @@ test("error user doesn't have owner permission ", () => {
     uId: authUserId3,
   });
 
-  let channelAddOwneRes = request(
-    "POST",
-    `${url}:${port}/channel/addowner/v1`,
-    {
-      token: token2,
-      channelID: channelId,
-      uId: authUserId3,
-    }
+  let channelAddOwneRes = requestChannelAddOwner(
+    token2,
+    channelId,
+    authUserId3
   );
+
   let { error } = JSON.parse(channelAddOwneRes);
   expect(error).toEqual(expect.any(String));
 });
