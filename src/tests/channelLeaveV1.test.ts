13283995a1beea81ade9f07edb2b2066304acf36
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-03-24 01:50:40 +1100

    finish implementing channelLeaveV1

--- src/tests/channelLeaveV1.test.ts
+++ src/tests/channelLeaveV1.test.ts
@@ -1,167 +1,84 @@
+import { channel } from "diagnostics_channel";
+import {
+  requestAuthLogin,
+  requestAuthRegister,
+  requestChannelDetails,
+  requestChannelInvite,
+  requestChannelLeave,
+  requestChannelsCreate,
+  requestClear,
+} from "../requests";
 import { port, url } from "./config.json";
 const request = require("sync-request");
 
-const OK = 200;
-
 beforeEach(() => {
-  request("DELETE", `${url}:${port}/clear/v1`);
+  requestClear();
 });
 
 test("success channel leave", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1 } = JSON.parse(loginRes.body);
-  let channelCreateRes = request("POST",`${url}:${port}/channels/create/v2`, {
-    json:{
-      token: token1,
-      name: "testing",
-      isPublic: true
-    }
-  });
-  let {channelId} = JSON.parse(channelCreateRes);
-  let channelRemoveRes = request("POST", `${url}:${port}/channel/leave/v1`, {
-    json: { token: token1, channelId: channelId}
-  })
-  expect(channelRemoveRes.statusCode).toBe(OK);
+  requestAuthRegister("olivrewlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("olivrewluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+
+  let loginRes1 = requestAuthLogin("olivrewlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+
+  let { token: token1 } = loginRes1;
+  let { authUserId: authUserId2, token: token2 } = loginRes2;
+  let channelCreateRes = requestChannelsCreate(token1, "testing", true);
+
+  let { channelId } = channelCreateRes;
+  requestChannelInvite(token1, channelId, authUserId2);
+  requestChannelLeave(token1, channelId);
+  let channelDetailRes = requestChannelDetails(token2, channelId);
+  let { allMembers } = channelDetailRes;
+  expect(allMembers.length).toEqual(1);
 });
 
 test("error invalid channelId", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1 } = JSON.parse(loginRes.body);
-  let channelCreateRes = request("POST",`${url}:${port}/channels/create/v2`, {
-    json:{
-      token: token1,
-      name: "testing",
-      isPublic: true
-    }
-  });
-  let {channelId} = JSON.parse(channelCreateRes);
-  let channelRemoveRes = request("POST", `${url}:${port}/channel/leave/v1`, {
-    json: { token: token1, channelId: channelId+1}
-  }) 
-  expect(channelRemoveRes.statusCode).toBe(400);
-  let {error} = JSON.parse(channelRemoveRes);
+  requestAuthRegister("olivrewlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("olivrewluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+  let loginRes1 = requestAuthLogin("olivrewlu@gmail.com", "cl3cl3vul4");
+  requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes1;
+
+  let channelCreateRes = requestChannelsCreate(token1, "testing", true);
+  let { channelId } = channelCreateRes;
+
+  let channelLeaveRes = requestChannelLeave(token1, channelId + 1);
+
+  let { error } = channelLeaveRes;
   expect(error).toEqual(expect.any(String));
 });
 
 test("error authUser not a part of channel", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1 } = JSON.parse(loginRes.body);
-  let { token: token2 } = JSON.parse(loginRes2.body);
-  let channelCreateRes = request("POST",`${url}:${port}/channels/create/v2`, {
-    json:{
-      token: token1,
-      name: "testing",
-      isPublic: true
-    }
-  });
-  let {channelId} = JSON.parse(channelCreateRes);
-  let channelRemoveRes = request("POST", `${url}:${port}/channel/leave/v1`, {
-    json: { token: token2, channelId: channelId}
-  }) 
-  expect(channelRemoveRes.statusCode).toBe(400);
-  let {error} = JSON.parse(channelRemoveRes);
+  requestAuthRegister("olivrewlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("olivrewluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+
+  let loginRes1 = requestAuthLogin("olivrewlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes1;
+  let { token: token2 } = loginRes2;
+  let channelCreateRes = requestChannelsCreate(token1, "testing", true);
+  let { channelId } = channelCreateRes;
+
+  let channelRemoveRes = requestChannelLeave(token2, channelId);
+
+  let { error } = channelRemoveRes;
   expect(error).toEqual(expect.any(String));
 });
 
 test("error invalid token", () => {
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwlu@gmail.com",
-      password: "cl3cl3vul4",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  request("POST", `${url}:${port}/auth/register/v2`, {
-    json: {
-      email: "oliverwluu@gmail.com",
-      password: "cl3cl3vul44",
-      nameFirst: "Oliver",
-      nameLast: "Lu",
-    },
-  });
-  let loginRes = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwlu@gmail.com", password: "cl3cl3vul4" },
-  });
-  let loginRes2 = request("POST", `${url}:${port}/auth/login/v2`, {
-    json: { email: "oliverwluu@gmail.com", password: "cl3cl3vul44" },
-  });
-  let { token: token1 } = JSON.parse(loginRes.body);
-  let channelCreateRes = request("POST",`${url}:${port}/channels/create/v2`, {
-    json:{
-      token: token1,
-      name: "testing",
-      isPublic: true
-    }
-  });
-  let {channelId} = JSON.parse(channelCreateRes);
-  let channelRemoveRes = request("POST", `${url}:${port}/channel/leave/v1`, {
-    json: { token: token1+"error", channelId: channelId}
-  })
-  expect(channelRemoveRes.statusCode).toBe(400);
-  let {error} = JSON.parse(channelRemoveRes);
+  requestAuthRegister("olivrewlu@gmail.com", "cl3cl3vul4", "Oliver", "Lu");
+  requestAuthRegister("olivrewluu@gmail.com", "cl3cl3vul44", "Oliver", "Lu");
+  let loginRes1 = requestAuthLogin("olivrewlu@gmail.com", "cl3cl3vul4");
+  let loginRes2 = requestAuthLogin("olivrewluu@gmail.com", "cl3cl3vul44");
+  let { token: token1 } = loginRes1;
+  let { token: token2 } = loginRes2;
+  let channelCreateRes = requestChannelsCreate(token1, "testing", true);
+  let { channelId } = channelCreateRes;
+
+  let channelLeaveRes = requestChannelLeave(token1 + "error", channelId);
+
+  let { error } = channelLeaveRes;
   expect(error).toEqual(expect.any(String));
-})
+});
