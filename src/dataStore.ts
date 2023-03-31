acff484983bee919ac5d004c7906b2bc21d46a02
Akanksha Sood <z5421482@ad.unsw.edu.au>
2023-03-31 21:26:12 +1100

    Update persistence

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -50,7 +50,15 @@ function fileSaveData() {
 
 // Updates the data based on the contents of dataStore.json
 export function fileLoadData() {
-  data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  // Check that the file exists locally
+  if(!fs.existsSync('data.json')) {
+    fileSaveData();
+  }
+  // The file *does* exist
+  else {
+    // Read the file
+    data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  }
 }
 
 export { getData, setData };
