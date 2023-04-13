05c4503b2b03f51b147c873aaa0294edaf9b2d05
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-04-13 16:42:23 +1000

    finish pwReset Test

--- src/tests/pwResetRequestV1.test.ts
+++ src/tests/pwResetRequestV1.test.ts
@@ -1,4 +1,4 @@
-import { requestClear, requestAuthRegister, reqeustPwReset, requestChannelsCreate } from "../requests";
+import { requestClear, requestAuthRegister, requestPwResetRequest, requestChannelsCreate, requestPwReset } from "../requests";
 
 beforeEach(() => {
     requestClear();
@@ -9,9 +9,16 @@ afterAll(() => {
 });
 
 
-test("successful reset request", () => {
+test("invalid reset code", () => {
     const {token} = requestAuthRegister('otonnokoo713@gmail.com', 'cl3cl3vul4', 'Oliver', 'Lu');
-    reqeustPwReset('otonnokoo713@gmail.com');
-    const res = requestChannelsCreate(token, "errorToken", true)
-    expect(res).toBe(403);
+    requestPwResetRequest('otonnokoo713@gmail.com');
+    const res = requestPwReset('error','jdiosajio')
+    expect(res).toBe(400);
+})
+
+test("newPassword Less than 6 characters long", () => {
+    const {token} = requestAuthRegister('otonnokoo713@gmail.com', 'cl3cl3vul4', 'Oliver', 'Lu');
+    requestPwResetRequest('otonnokoo713@gmail.com');
+    const res = requestPwReset('error','jdio')
+    expect(res).toBe(400);
 })
