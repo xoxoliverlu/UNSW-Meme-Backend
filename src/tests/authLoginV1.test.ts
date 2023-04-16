b4ff35f8df459d304d69ebee6ee3a0de9106441b
Akanksha Sood <z5421482@ad.unsw.edu.au>
2023-04-16 16:32:44 +1000

    Update tests for authLoginV3

--- src/tests/authLoginV1.test.ts
+++ src/tests/authLoginV1.test.ts
@@ -9,17 +9,13 @@ afterAll(() => {
 });
 
 describe('Auth Login Valid inputs', () => {
-  test('Valid return type (object)', () => {
+  test('Valid return type (object) with valid userId', () => {
     const register1 = requestAuthRegister('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
     const login1 = requestAuthLogin('alice.smith@gmail.com', '123456');
     expect(login1).toStrictEqual({
       token: expect.any(String),
       authUserId: register1.authUserId,
     });
-  });
-  test('Valid userId', () => {
-    const register1 = requestAuthRegister('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
-    const login1 = requestAuthLogin('alice.smith@gmail.com', '123456');
     expect(register1.authUserId).toEqual(login1.authUserId);
   });
   test('Same user can create unique sessions', () => {
@@ -42,12 +38,12 @@ describe('Auth Login Valid inputs', () => {
 describe('Invalid inputs', () => {
   test('Email does not belong to a user', () => {
     requestAuthRegister('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
-    const login1 = requestAuthLogin('bob.langford@gmail.com', '123456');
-    expect(login1).toEqual({ error: expect.any(String) });
+    const login1 = requestAuthLogin('bob.langford@gmail.com', '123456')
+    expect(login1).toStrictEqual(400);
   });
   test('Incorrect password', () => {
     requestAuthRegister('alice.smith@gmail.com', '123456', 'Alice', 'Smith');
     const login1 = requestAuthLogin('alice.smith@gmail.com', 'password');
-    expect(login1).toEqual({ error: expect.any(String) });
+    expect(login1).toStrictEqual(400);
   });
 });
