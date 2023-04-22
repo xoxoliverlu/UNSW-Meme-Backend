b37f04e91886d751788b47c90e5265e45ee4b0ba
Wei-Cheng, Lu <z5295931@ad.unsw.edu.au>
2023-04-22 13:38:53 +1000

    finish test for userStatsV1

--- src/tests/userStatsV1.test.ts
+++ src/tests/userStatsV1.test.ts
@@ -1,4 +1,4 @@
-import { requestChannelsCreate, requestAuthRegister, requestMessageSend, requestClear, requestUserStats } from '../requests';
+import { requestChannelsCreate, requestAuthRegister, requestMessageSend, requestClear, requestUserStats, requestDmCreate, requestMessageSendDm } from '../requests';
 
 beforeEach(() => {
   requestClear();
@@ -11,13 +11,16 @@ afterAll(() => {
 describe('Successful', () => {
   test('involvement Rate = 1', () => {
     const register = requestAuthRegister('dimpi.garnepudi@gmail.com', 'dimpi123', 'Dimpi', 'Garnepudi');
+    const register2 = requestAuthRegister('oilverwlu@gmail.com', 'cl3cl3vul4', 'Oliver', 'Lu');
     const channel = requestChannelsCreate(register.token, 'channelNew', true);
-    const message = requestMessageSend(register.token, channel.channelId, 'cat');
+    const {dmId} = requestDmCreate(register.token,[register2.token]);
+    requestMessageSend(register.token, channel.channelId, 'cat');
+    requestMessageSendDm(register2.token,dmId, "Hello");
     const {userStats} = requestUserStats(register.token)
     expect(userStats).toHaveProperty('channelsJoined');
     expect(userStats).toHaveProperty('dmsJoined');
     expect(userStats).toHaveProperty('messagesSent');
     expect(userStats).toHaveProperty('involvementRate');
-    expect(userStats.involvementRate).toBe(1);
+    expect(userStats.involvementRate).toBe((1+1+1/1+1+2));
   });
 });
